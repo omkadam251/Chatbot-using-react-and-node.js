a423db4b84be37bb4a87d51fd2f401539ca93350
Omkadam <omkadam251@gmail.com>
2019-04-10 20:12:13 +0530

    connect server side and dialogflow

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
+const bodyParser=require('body-parser');
+
 const app=express();
 
-app.get('/',(req,res) =>{
-	res.send({'Hello':'there'});
-});
+app.use(bodyParser.json());
+
+require('./routes/dialogFlowRoutes')(app);
+
 
 const PORT=process.env.PORT || 5000;
 process.env.PORT || 5000;
