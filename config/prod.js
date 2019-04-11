1d973ce4e743bc58428d8a5b5a7e71c592995039
Omkadam <omkadam251@gmail.com>
2019-04-11 16:35:38 +0530

    ch

--- config/prod.js
+++ config/prod.js
@@ -3,7 +3,7 @@ module.exports = {
 	googleProjectID: process.env.GOOGLE_PROJECT_ID,
 	dialogFlowSessionID: process.env.DIALOGFLOW_SESSION-ID ,
 	dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE
-	googleClientEmail: JSON.parse(process.env.GOOGLE_CLIENT_EMAIL),
-	googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
+	Google_Client_Email: JSON.parse(process.env.GOOGLE_CLIENT_EMAIL),
+	Google_Private_Key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 
 }
